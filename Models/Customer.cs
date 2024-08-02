1fe5699524c518cbd8305bcfbc06bd5b484cc87c
carobte <caro.bustamante.escobar@gmail.com>
2024-08-02 07:56:05 -0500

    feat: Customer class

---
+++ Models/Customer.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace simulacro_csharp.Models
+{
+    public class Customer : User
+    {
+        public string? MembershipLevel { get; set; }
+        public string? PreferredPaymentMethod { get; set; }
+
+        public Customer(string name, string lastName, string typeDocument, string identificationNumber, DateOnly birthdate, string email, string phoneNumber, string address, string membershipLevel, string preferredPaymentMethod) : base(name, lastName, typeDocument, identificationNumber, birthdate, email, phoneNumber, address)
+        {
+            MembershipLevel = membershipLevel;
+            PreferredPaymentMethod = preferredPaymentMethod;
+        }
+
+        public void UpdateMembershipLevel(string newMembershipLevel)
+        {
+            MembershipLevel = newMembershipLevel;
+        }
+
+    }
+}
