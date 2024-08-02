270a7a902d27d561ab1999f362fc4ef7f57e6586
carobte <caro.bustamante.escobar@gmail.com>
2024-08-02 06:55:46 -0500

    feat: vehicle class

--- Models/Vehicle.cs
+++ Models/Vehicle.cs
@@ -7,6 +7,26 @@ namespace simulacro_csharp.Models
 {
     public class Vehicle
     {
-        
+        public int Id { get; set; }
+        public string? Placa { get; set; }
+        public string? Tipo { get; set; }
+        public string? EngineNumber { get; set; }
+        public string? SerialNumber { get; set; }
+        public byte PeopleCapacity { get; set; }
+
+        public Vehicle(int id, string placa, string tipo, string engineNumber, string serialNumber, byte peopleCapacity){
+            Id = id;
+            Placa = placa;
+            Tipo = tipo;
+            EngineNumber = engineNumber;
+            SerialNumber = serialNumber;
+            PeopleCapacity = peopleCapacity;
+        }
+
+        public void DeleteVehicle(int id){
+            
+        }
+
+
     }
 }
